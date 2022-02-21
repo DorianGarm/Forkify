75fa9ba532efd6809a6e6404e87a77f32912659d
Dorian Sobołtyński <68549661+DorianGarm@users.noreply.github.com>
2022-02-21 17:17:12 +0100

    add features: bookmarks and localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -16,17 +17,19 @@ const controlRecipies = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
     recipeView.renderSpinner();
+    resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
     await model.loadRecipe(id);
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
 const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
-    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
@@ -47,8 +50,34 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Udate the recipe servings
+  model.updateServings(newServings);
+  // Update the view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) Add a bookmark if not yet bookmarked
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  // 2) or delete a bookmark if bookmarked already
+  else model.deleteBookmark(model.state.recipe.id);
+  // 3) Update the recipe view
+  recipeView.update(model.state.recipe);
+  // 4) Render bookmarks menu
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipies);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
